3f903508f22376c25dc640be03c2b0989ac80127
KamauDev-maker <oscarnjenga@gmail.com>
2023-10-20 17:19:03 +0000

    simple pagnation

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 """
-Simple pagination
+Simple pagnation
 """
+from typing import List, Tuple
 import csv
 import math
-from typing import Tuple, List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Method that takes two integer args
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """
+    Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -16,8 +26,6 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -27,18 +35,13 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
+        """
+        Retrieves the data
         """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
-
-        range: Tuple = index_range(page, page_size)
-        pagination: List = self.dataset()
-
-        return (pagination[range[0]:range[1]])
-
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """
-        Method that takes two int and returns a tuple
-        """
-        return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+        dataset = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+        if start_index >= len(dataset):
+            return []
+        return dataset[start_index:end_index]
