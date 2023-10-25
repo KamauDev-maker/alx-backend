16a3197979ae6f6059774f6d3c01a4a00d8b6eac
KamauDev-maker <oscarnjenga@gmail.com>
2023-10-25 03:05:27 +0000

    LIFO caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python3
 """
-FIFO caching
+LIFO cache
 """
+
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """
-    Class that inherits from Basecaching
+    Class that inherits from BaseCaching
     """
     def __init__(self):
         """
-        Method that initializes
+        Method that initializes class instance
         """
         super().__init__()
         self.keys = []
 
     def put(self, key, item):
         """
-        Method that add or updates an item in cache
+        Method that add or updates a cache items
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
             if key not in self.keys:
                 self.keys.append(key)
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
             if len(self.keys) > BaseCaching.MAX_ITEMS:
-                first_key = self.keys.pop(0)
-                del self.cache_data[first_key]
-                print('DISCARD: {:s}'. format(first_key))
+                last_key = self.keys.pop(-2)
+                del self.cache_data[last_key]
+                print('DISCARD: {:s}'.format(last_key))
 
     def get(self, key):
         """
-        Method that retrieves the cache data
+        Method that retrieves a cache item
         """
         if key is not None:
-            return self.cache_data.get(key)
+            return self.cache_data[key]
         return None
